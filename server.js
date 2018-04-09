5327ea3006f93236a4397d2a1e9b7bd717599a30
huynhsamha <huynhsamha@gmail.com>
2018-04-09 07:41:57 +0700

    fix project

---
+++ server.js
@@ -0,0 +1,69 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var engine = require('ejs-locals');
+
+var index = require('./routes/index');
+var user = require('./routes/user');
+var film = require('./routes/film');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('ejs', engine);
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/user', user);
+app.use('/film', film);
+
+// handle all requests to the build folder
+// app.get('*', (req, res, next) => {
+//   res.sendFile(path.join(__dirname, './build/index.html'));
+// });
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+var server = http.createServer(app);
+var port = process.env.PORT || '3000';
+app.set('port', port);
+
+server.listen(port, () => console.log(`Running on localhost:${port}`));
+
+/**
+ * Config interval timer ping app to prevent sleeping
+ */
+const HALF_HOURS = 1000 * 60 * 30;
+
+setInterval(() => {
+  http.get('https://app-cinema-website.herokuapp.com/');
+}, HALF_HOURS);
